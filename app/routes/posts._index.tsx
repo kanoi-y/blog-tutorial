d95ef9613d6d5fd87cd2d2e1935b123174505eb8
kanoi-y <furafuratonbo@gmail.com>
2023-06-18 17:56:31 +0900

    ✨ admin画面を作成

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,7 +1,5 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-
-
 import { getPosts } from "~/models/post.server";
 
 export const loader = async () => {
@@ -13,13 +11,13 @@ export default function Posts() {
   return (
     <main>
       <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link
-              to={post.slug}
-              className="text-blue-600 underline"
-            >
+            <Link to={post.slug} className="text-blue-600 underline">
               {post.title}
             </Link>
           </li>
